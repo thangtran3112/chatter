29a905318e169a61e25db974251ab10d19a74b7a
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-02-18 09:29:18 -0800

    Clean up and add eslint prettier

--- README.md
+++ README.md
@@ -3,6 +3,13 @@
 `yarn add @mui/material @emotion/react @emotion/styled @fontsource/roboto @mui/icons-materi
 al`
 
+## Install eslint for typescript
+
+https://andrebnassis.medium.com/setting-eslint-on-a-react-typescript-project-2021-1190a43ffba
+https://dev.to/quizzes4u/how-to-set-up-eslint-and-prettier-in-react-typescript-5-project-2023-hd6
+
+`npx eslint --init`
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+} from '@mui/material';
 
-function App() {
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+/**Container allow auto element spacing and justify-center */
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container>
+        <h1>Dark Mode</h1>
+      </Container>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
