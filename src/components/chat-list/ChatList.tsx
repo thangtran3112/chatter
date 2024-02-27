e9b589f9f9532b3a755efd237e67b9474c37170a
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-02-26 19:22:52 -0800

    Scroll to bottom effect and display Grid of messages

--- src/components/chat-list/ChatList.tsx
+++ src/components/chat-list/ChatList.tsx
@@ -3,13 +3,28 @@ import ChatListItem from './chat-list-item/ChatListItem';
 import Stack from '@mui/material/Stack';
 import ChatListHeader from './chat-list-header/ChatListHeader';
 import { Divider } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ChatListAdd from './chat-list-add/ChatListAdd';
 import { useGetChats } from '../../hooks/useGetChats';
+import { usePath } from '../../hooks/usePath';
 
 export default function ChatList() {
   const [chatListAddVisible, setChatListAddVisible] = useState(false);
   const { data } = useGetChats();
+  const [selectedChatId, setSelectedChatId] = useState('');
+  //we could use useLocation, but we are outside of <Router>
+  const { path } = usePath();
+
+  /**
+   * Every time the path is changed, we are setting the selectedChatId from path
+   * Example of path: /chats/65dd4b43ac8ff3f1ab09f9b4
+   */
+  useEffect(() => {
+    const pathSplit = path.split('chats/');
+    if (pathSplit.length === 2) {
+      setSelectedChatId(pathSplit[1]);
+    }
+  }, [path]);
 
   return (
     <>
@@ -29,9 +44,16 @@ export default function ChatList() {
             overflow: 'auto', //scrollable through the overflow list
           }}
         >
-          {data?.chats.map((chat) => (
-            <ChatListItem chat={chat} key={chat._id} />
-          ))}
+          {data?.chats
+            .map((chat) => (
+              <ChatListItem
+                selected={chat._id === selectedChatId}
+                chat={chat}
+                key={chat._id}
+              />
+            ))
+            .reverse()}
+          {/* reverse to make sure newest Chat show up on top */}
         </List>
       </Stack>
     </>
