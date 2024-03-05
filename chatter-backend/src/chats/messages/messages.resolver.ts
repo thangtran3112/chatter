d2d50a56facb922d3aeb64c7633bf93257d65d17
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-03-04 16:33:07 -0800

    GraphQL Subscription for message

--- chatter-backend/src/chats/messages/messages.resolver.ts
+++ chatter-backend/src/chats/messages/messages.resolver.ts
@@ -1,16 +1,23 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
 import { MessagesService } from './messages.service';
 import { Message } from './entities/message.entity';
-import { UseGuards } from '@nestjs/common';
+import { Inject, UseGuards } from '@nestjs/common';
 import { GqlAuthGuard } from 'src/auth/guards/gql-auth.guard';
 import { CreateMessageInput } from './dto/create-message.input';
 import { CurrentUser } from 'src/auth/current-user.decorator';
 import { TokenPayload } from 'src/auth/token-payload.interface';
 import { GetMessagesArgs } from './dto/get-messages.args';
+import { PUB_SUB } from 'src/common/constants/injection-tokens';
+import { PubSub } from 'graphql-subscriptions';
+import { MESSAGE_CREATED_TOPIC } from './constants/pubsub';
+import { MessageCreatedArgs } from './dto/message-created.args';
 
 @Resolver(() => Message)
 export class MessagesResolver {
-  constructor(private readonly messagesService: MessagesService) {}
+  constructor(
+    private readonly messagesService: MessagesService,
+    @Inject(PUB_SUB) private readonly pubSub: PubSub,
+  ) {}
 
   @Mutation(() => Message)
   @UseGuards(GqlAuthGuard)
@@ -28,5 +35,19 @@ export class MessagesResolver {
     @CurrentUser() user: TokenPayload,
   ) {
     return this.messagesService.getMessages(getMessagesArgs, user._id);
+  }
+
+  @Subscription(() => Message, {
+    //filter for corresponding messages
+    filter: (payload, variables) => {
+      //incoming message (payload.message) to be compared with the subscription chatId
+      //this API will be called by UI client with { chatId } to subscribe to the topic
+      return payload.messageCreated.chatId === variables.chatId;
+    },
+  })
+  messageCreated(@Args() _messageCreatedArgs: MessageCreatedArgs) {
+    //topic name for this specific message type
+    //which message go to which subscription by the trigger name
+    return this.pubSub.asyncIterator(MESSAGE_CREATED_TOPIC);
   }
 }
--- chatter-backend/src/chats/messages/messages.service.ts
+++ chatter-backend/src/chats/messages/messages.service.ts
@@ -1,18 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ChatsRepository } from '../chats.repository';
 import { CreateMessageInput } from './dto/create-message.input';
 import { Types } from 'mongoose';
 import { Message } from './entities/message.entity';
 import { GetMessagesArgs } from './dto/get-messages.args';
+import { PUB_SUB } from 'src/common/constants/injection-tokens';
+import { PubSub } from 'graphql-subscriptions';
+import { MESSAGE_CREATED_TOPIC } from './constants/pubsub';
 
 @Injectable()
 export class MessagesService {
-  constructor(private readonly chatsRepository: ChatsRepository) {}
+  constructor(
+    private readonly chatsRepository: ChatsRepository,
+    @Inject(PUB_SUB) private readonly pubSub: PubSub,
+  ) {}
 
   async createMessage({ content, chatId }: CreateMessageInput, userId: string) {
     const message: Message = {
       content,
       userId,
+      chatId,
       createdAt: new Date(),
       _id: new Types.ObjectId(),
     };
@@ -29,6 +36,12 @@ export class MessagesService {
         },
       }, //after finding the Chat objects, we are updating by pushing new message into messages array
     );
+
+    //after creating the message in database, we publish the message to pubSub
+    const payload = {
+      messageCreated: message,
+    };
+    await this.pubSub.publish(MESSAGE_CREATED_TOPIC, payload);
 
     return message;
   }
