343938b6a8480b7fa7c6bfaf160dab5df6a67dc8
thangtran3112 <37521149+thangtran3112@users.noreply.github.com>
2024-03-07 17:40:43 -0800

    Cached LatestMessage on Create Chat and Subscription

--- chatter-ui/src/hooks/useCreateMessage.ts
+++ chatter-ui/src/hooks/useCreateMessage.ts
@@ -2,6 +2,7 @@
 import { useMutation } from '@apollo/client';
 import { graphql } from '../gql';
 import { updateMessages } from '../cache/messages';
+import { updateLatestMessage } from '../cache/latest-message';
 
 const createMessageDocument = graphql(`
   mutation CreateMessage($createMessageInput: CreateMessageInput!) {
@@ -22,6 +23,7 @@ export const useCreateMessage = () =>
       const newMessage = data?.createMessage;
       if (newMessage) {
         updateMessages(cache, newMessage);
+        updateLatestMessage(cache, newMessage);
       }
     },
   });
